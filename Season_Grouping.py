ccca0a87026b4119804ca4a0165ab9558368af15
DrewClark123-dev <109322451+DrewClark123-dev@users.noreply.github.com>
2026-07-13 00:17:55 -0500

    Dummy main page

---
+++ Season_Grouping.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+if __name__ == "__main__":
+    print("==================================================")
+    st.sidebar.title("Navigation")
+    st.sidebar.markdown("Select a page from the sidebar to navigate.")
+    st.set_page_config(layout="wide", page_title="Lastwar AI")
+    st.markdown("<h1 style='text-align: center; color: #3ea6ff; '>OLDs Lastwar Dashboard</h1>", unsafe_allow_html=True)
+    st.write("")
+    
